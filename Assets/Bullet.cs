55d14f4e33cb728a2c932af27dd553b77c32a2dc
Vadym Strelchenko <bowlingforsoap@mail.ru>
2018-06-21 17:14:43 +1200

    Networked shooting and health

---
+++ Assets/Bullet.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine;
+
+public class Bullet : NetworkBehaviour {
+	void OnCollisionEnter(Collision collision) {
+		Health health = collision.gameObject.GetComponent<Health>();
+		if (health != null) {
+			health.TakeDamage(10);
+		}
+
+		Destroy(gameObject);
+	}
+}
